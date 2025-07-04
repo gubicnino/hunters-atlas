48dbeaf233a0c9e432dec818a1f70323362534b2
Nino Gubič <gubicnino@gmail.com>
2025-07-04 14:19:52 +0200

    Add SSL configuration for PostgreSQL connection to support Vercel deployment

--- db.js
+++ db.js
@@ -2,6 +2,7 @@ const postgres = require('postgres');
 
 const connectionString = process.env.DATABASE_URL;
 console.log('Connecting to database with connection string:', connectionString);
-const sql = postgres(connectionString);
-
+const sql = postgres(connectionString, {
+  ssl: { rejectUnauthorized: false }  // Add this for Vercel
+});
 module.exports = sql;
